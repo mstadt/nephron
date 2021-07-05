ff5be90b8c81d9a2ae608e6e96ea444bdacd31ce
Melissa Stadt <84604412+mstadt@users.noreply.github.com>
2021-07-05 16:52:26 -0700

    Update Newton_preg.py
    newton_preg_rat is same as the rat model for now

--- Newton_preg.py
+++ Newton_preg.py
@@ -4,9 +4,11 @@ import numpy as np
 from Newton import Jac
 
 def newton_preg_rat(func,x,k,cell):
-    print('to do')
     if cell.humOrrat != 'rat':
-        raise Exception('newton_rat only for rat model')
+        raise Exception('newton_preg_rat only for rat model')
+    if cell.sex != 'Female':
+        print('sex: ' + cell.sex)
+        raise Exception('pregnant only for female rat')
     fun=equations.conservation_eqs
     f=np.matrix(fun(x,k))
     TOLpcn = 1
@@ -17,30 +19,101 @@ def newton_preg_rat(func,x,k,cell):
         J = np.matrix(Jac(fun,x,k))
         IJ = J.I
         F = np.matrix(fun(x,k))
+        # PCT
         if cell.segment == 'PT':
             amp = 1.0
+        # S3
         elif cell.segment == 'S3':
-            amp = 1.0
+            amp = 1.0 
+        # SDL
         elif cell.segment == 'SDL':
-            amp = 1.0
+            amp = 0.2
+        # LDL
         elif cell.segment == 'LDL':
-            amp = 1.0
+            if np.linalg.norm(f)>5000:
+                amp = 0.5
+            else:
+                amp = 1.0
+        # LAL
         elif cell.segment == 'LAL':
-            amp = 1.0
+            if np.linalg.norm(f)>5000:
+                amp = 0.5
+            else:
+                amp = 1.0
+        # mTAL
         elif cell.segment == 'mTAL':
-            amp = 1.0
+            if np.linalg.norm(f)>100:
+                amp = 0.2
+            else:
+                amp = 0.9
+        # cTAL
         elif cell.segment == 'cTAL':
-            amp = 1.0
+            if np.linalg.norm(f)>100:
+                amp = 0.2
+            else:
+                amp = 0.8
+        # DCT
         elif cell.segment == 'DCT':
             amp = 1.0
+        # CNT
         elif cell.segment == 'CNT':
-            amp = 1.0
+            if cell.sex == 'female':
+                if cell.type == 'sup':
+                    if np.linalg.norm(f)>100:
+                        amp = 0.17
+                    else:
+                        amp = 0.9
+                elif cell.type == 'jux1':
+                    if np.linalg.norm(f)>5000:
+                        if k==0:
+                            amp = 0.4
+                        else:
+                            amp = 0.3
+                    else:
+                        amp = 0.9
+                else:
+                    if np.linalg.norm(f) > 5000:
+                        amp = 0.13
+                    else:
+                        amp = 0.9
+            elif cell.sex == 'male':
+                if cell.type == 'sup':
+                    if np.linalg.norm(f)>5000:
+                        amp = 0.5
+                    else:
+                        amp = 0.8
+                else:
+                    if np.linalg.norm(f)>5000:
+                        amp = 0.13
+                    else:
+                        amp = 1.0
+        # CCD     
         elif cell.segment == 'CCD':
-            amp = 1.0
+            if np.linalg.norm(f)>1000:
+                if k==0:
+                    amp = 0.3
+                else:
+                    amp = 0.1
+            else:
+                amp = 0.8
+        # OMCD
         elif cell.segment == 'OMCD':
-            amp = 1.0
+            if np.linalg.norm(f)>100:
+                amp = 0.8
+            else:
+                amp = 1.0
+        # IMCD
         elif cell.segment == 'IMCD':
-            amp = 1.0
+            if np.linalg.norm(f)>100:
+                if k==0:
+                    amp = 0.2
+                else:
+                    amp = 0.1
+            else:
+                if k==0:
+                    amp = 0.5
+                else:
+                    amp = 0.9
         else:
             print('What is this segment?', cell.segment)
             raise Exception('cell.segment is not characterized')
