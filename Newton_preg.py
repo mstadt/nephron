a079c5ad7d5dae8eb5e3b3966827e075a7e902cb
Melissa Stadt <84604412+mstadt@users.noreply.github.com>
2021-08-23 16:17:58 -0700

    updated Newton_preg

--- Newton_preg.py
+++ Newton_preg.py
@@ -142,18 +142,31 @@ def newton_preg_rat(func,x,k,cell):
                     amp = 0.8
         # OMCD
         elif cell.segment == 'OMCD':
-            if np.linalg.norm(f)>1000:
+            if np.linalg.norm(f)>5000:
                 if cell.preg == 'mid':
                     amp = 0.1
                 elif cell.preg == 'late':
                     amp = 0.1
-            elif np.linalg.norm(f)>100:
+            elif np.linalg.norm(f)>1000:
+                if cell.preg == 'mid':
+                    amp = 0.5
+                elif cell.preg == 'late':
+                    amp = 0.5
+            elif np.linalg.norm(f)>500:
                 if cell.preg == 'mid':
                     amp = 0.7
+                elif cell.preg == 'late':
+                    amp = 0.6
+            elif np.linalg.norm(f)>100:
+                if cell.preg == 'mid':
+                    amp = 0.8 #0.7
                 elif cell.preg == 'late':
                     amp = 0.7
             else:
-                amp = 0.9
+                if cell.preg == 'mid':
+                    amp = 1.0
+                elif cell.preg == 'late':
+                    amp = 0.8 #0.9
         # IMCD
         elif cell.segment == 'IMCD':
             if cell.preg == 'mid':
