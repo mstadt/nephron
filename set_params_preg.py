504f529bbc7fe9b4c60ff53068a8601d76fe579b
Melissa Stadt <84604412+mstadt@users.noreply.github.com>
2021-09-06 13:59:39 -0400

    set back to 1.8

--- set_params_preg.py
+++ set_params_preg.py
@@ -440,7 +440,7 @@ def read_params_preg(cell,filename,j):
                         raise Exception('NaKATPase activity not done for pregnancy in segment')
                 elif newTransp.type == 'NKCC2A' or newTransp.type == 'NKCC2B' or newTransp.type == 'NKCC2F':
                     if cell.preg == 'mid':
-                        preg_rat = 1.4 #1.8
+                        preg_rat = 1.8
                     elif cell.preg == 'late':
                         preg_rat = 2.0
                 elif newTransp.type == 'KCC4':
