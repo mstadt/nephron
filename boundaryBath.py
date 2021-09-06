f3c36d2ae79db66b8a7bdd1da7f55fbe0b0c6d6d
Melissa Stadt <84604412+mstadt@users.noreply.github.com>
2021-09-06 18:06:36 -0400

    Cl also decreased by 95% in the plasma

--- boundaryBath.py
+++ boundaryBath.py
@@ -8,13 +8,15 @@ def female_conc(cell, i):
     cell.conc[2,5]=cell.conc[2,5]-1
     if cell.segment == 'PT':
         if cell.preg == 'mid':
-            # conc of Na, K different in MP rat plasma
+            # conc of Na, K, Cl different in MP rat plasma
             cell.conc[0,:] = cell.conc[0,:]*0.95
             cell.conc[1,:] = cell.conc[1,:]*1.2
+            cell.conc[2,:] = cell.conc[2,:]*0.95
         elif cell.preg == 'late':
-            # conc of Na, K different in LP rat plasma
+            # conc of Na, K, Cl different in LP rat plasma
             cell.conc[0,:] = cell.conc[0,:]*0.95
             cell.conc[1,:] = cell.conc[1,:]*1.3
+            cell.conc[2,:] = cell.conc[2,:]*0.95
     
 
 def boundaryBath(cell,i):
