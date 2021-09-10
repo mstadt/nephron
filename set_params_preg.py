7adcf0496db8e701ec19cac3fb8f4a68725071e8
Melissa Stadt <84604412+mstadt@users.noreply.github.com>
2021-09-10 09:26:53 -0400

    Update set_params_preg.py

--- set_params_preg.py
+++ set_params_preg.py
@@ -375,7 +375,7 @@ def read_params_preg(cell,filename,j):
                             newdLA.perm = 0.9*newdLA.perm 
                         elif cell.preg == 'late':
                             newdLA.perm = 0.85*newdLA.perm
-                if cell.segment == 'DCT':
+                elif cell.segment == 'DCT':
                     # K-Cl cotransporter
                     if newdLA.solute_id == (1,2):
                         if cell.preg == 'late':
