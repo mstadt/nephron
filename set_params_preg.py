53005b368805293730f7a635ae7d374cbc6fbcb1
Melissa Stadt <84604412+mstadt@users.noreply.github.com>
2021-09-18 11:42:05 -0400

    Update set_params_preg.py

--- set_params_preg.py
+++ set_params_preg.py
@@ -540,7 +540,7 @@ def read_params_preg(cell,filename,j):
                         preg_rat = 0.85
                 elif newTransp.type == 'AE1':
                     if cell.preg == 'mid':
-                        preg_rat = 1.0
+                        preg_rat = 1.25
                     elif cell.preg == 'late':
                         preg_rat = 1.0
                 else:
