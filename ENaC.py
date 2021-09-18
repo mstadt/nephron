16bfcb3534247df25255a00f1774096e84787f27
Melissa Stadt <84604412+mstadt@users.noreply.github.com>
2021-09-17 22:21:45 -0400

    Update ENaC.py

--- ENaC.py
+++ ENaC.py
@@ -40,22 +40,22 @@ def ENaC(cell,i,memb_id,hNaMP,area,jvol):
             NaMPq0=cell.vol_init[0]-(2.0e-6)/60/Vref
         facFvMP=max(0.01,1+3*((cell.vol[0]/NaMPq0)-1))
         
-        # tracking
-        fname = 'tracking_all.txt'
-        f0 = open(fname, 'a')
-        f0.write('cell.vol_init[0]: ' + str(cell.vol_init[0]) + ', flow_ref/60/Vref: ' + str((flow_ref/60/Vref)) + ', cell_vol[0]: '+ str(cell.vol[0]) + ', NaMPq0: '+str(NaMPq0)+ ', cell_vol[0]/NaMPq0: '+str(cell.vol[0]/NaMPq0) + '\n')
-        f0.close()
+        # # tracking
+        # fname = 'tracking_all.txt'
+        # f0 = open(fname, 'a')
+        # f0.write('cell.vol_init[0]: ' + str(cell.vol_init[0]) + ', flow_ref/60/Vref: ' + str((flow_ref/60/Vref)) + ', cell_vol[0]: '+ str(cell.vol[0]) + ', NaMPq0: '+str(NaMPq0)+ ', cell_vol[0]/NaMPq0: '+str(cell.vol[0]/NaMPq0) + '\n')
+        # f0.close()
 
 
-        fname1 = 'tracking_NaMPq0.txt'
-        f1 = open(fname1, 'a')
-        f1.write(str(NaMPq0)+'\n')
-        f1.close()
+        # fname1 = 'tracking_NaMPq0.txt'
+        # f1 = open(fname1, 'a')
+        # f1.write(str(NaMPq0)+'\n')
+        # f1.close()
 
-        fname2 = 'tracking_facFvMP.txt'
-        f2 = open(fname2, 'a')
-        f2.write(str(facFvMP) + '\n')
-        f2.close()
+        # fname2 = 'tracking_facFvMP.txt'
+        # f2 = open(fname2, 'a')
+        # f2.write(str(facFvMP) + '\n')
+        # f2.close()
 
         hENaC=hNaMP*facNaMP*facCaMP*facphMP*facFvMP
 
