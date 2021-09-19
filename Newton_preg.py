ca9b411ed1dd51d12d57760e6e7fd3e5a58f40f9
Melissa Stadt <84604412+mstadt@users.noreply.github.com>
2021-09-19 16:25:01 -0400

    Update Newton_preg.py

--- Newton_preg.py
+++ Newton_preg.py
@@ -18,8 +18,9 @@ def newton_preg_rat(func,x,k,cell):
     while(np.linalg.norm(f) > 0.0001): #and (iter<150): 
         if np.linalg.norm(f)>1e12 or np.isnan(np.linalg.norm(f)):
             raise Exception('Newton solver diverged in '+ cell.segment + ' at cell number: ' + str(k))
-        elif iter>150:
-            raise Exception('Newton solver did not converge in <'+iter+' iterations in ' + cell.segment + ' cell number ' + str(k))
+        elif iter>200:
+            print('Warning!!: Newton solver did not converge in <'+str(iter)+' iterations in ' + cell.segment + ' cell number ' + str(k)')
+            #raise Exception('Newton solver did not converge in <'+str(iter)+' iterations in ' + cell.segment + ' cell number ' + str(k))
 
         i += 1
         J = np.matrix(Jac(fun,x,k))
