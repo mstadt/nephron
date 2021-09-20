aaa98b8db6f30091ee302d2fc042b768138c43ff
Melissa Stadt <84604412+mstadt@users.noreply.github.com>
2021-09-19 21:28:36 -0400

    Update set_params_preg.py

--- set_params_preg.py
+++ set_params_preg.py
@@ -406,12 +406,12 @@ def read_params_preg(cell,filename,j):
                 elif newdLA.solute_id == (0,2):
                     if cell.segment == 'OMCD':
                         if cell.preg == 'mid':
-                            newdLA.perm = 1.0*newdLA.perm
+                            newdLA.perm = 1.05*newdLA.perm
                         elif cell.preg == 'late':
                             newdLA.perm = 1.1*newdLA.perm
                     elif cell.segment == 'IMCD':
                         if cell.preg == 'mid':
-                            newdLA.perm = 1.0*newdLA.perm
+                            newdLA.perm = 1.05*newdLA.perm
                         elif cell.preg == 'late':
                             newdLA.perm = 1.1*newdLA.perm
                     else:
