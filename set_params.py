983aa978c48a26e54f76ab0ce7bc116a8fd3ccfc
Melissa Stadt <84604412+mstadt@users.noreply.github.com>
2021-09-30 14:38:05 -0400

    Update set_params.py

--- set_params.py
+++ set_params.py
@@ -131,10 +131,7 @@ def read_params(cell,filename,j):
             elif compare_string_prefix(id,"Diameter"):
                 # diabetic diameter
                 if cell.diabete == 'Non':
-                    if cell.type == 'sup':
-                        cell.diam = value
-                    else:
-                        cell.diam = value*1.05
+                    cell.diam = value
                 elif cell.diabete == 'Moderate':
                     if cell.segment == 'PT' or cell.segment == 'S3':
                         cell.diam = value*1.1
@@ -274,7 +271,7 @@ def read_params(cell,filename,j):
             elif compare_string_prefix(id,"Pressure"):
                 cell.pres[0] = value
                 
-            # parameter files specify pressure (value) for superficial nephrons; we manually specify juxtamedullary pressure values...
+            # parameter files specify pressure (value) for superficial nephrons
                 if cell.type !='sup' and cell.segment == 'PT' and cell.species == 'rat':
                     if cell.sex == 'male':
                         cell.pres[0] = 12.5
