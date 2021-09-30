3bcbe84aa40d1bfd17879556627c0cb67fca1ca5
Melissa Stadt <84604412+mstadt@users.noreply.github.com>
2021-09-30 12:40:02 -0400

    Update Newton.py

--- Newton.py
+++ Newton.py
@@ -228,12 +228,12 @@ def newton_rat(func,x,k,cell):
                 if k==0:
                     amp = 0.25
                 else:
-                    amp = 0.5
+                    amp = 0.3 #0.5
             elif np.linalg.norm(f)>1000:
                 if k==0:
                     amp = 0.5
                 else:
-                    amp = 0.75
+                    amp = 0.6 #0.75
             elif iter>75:
                 amp = 0.7
             elif iter>50:
