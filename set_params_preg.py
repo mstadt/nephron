2c6166b120139e72dcd61dd79a1a52a8ac588b59
Melissa Stadt <84604412+mstadt@users.noreply.github.com>
2021-10-17 10:59:58 -0400

    Update set_params_preg.py

--- set_params_preg.py
+++ set_params_preg.py
@@ -343,28 +343,28 @@ def read_params_preg(cell,filename,j):
                             preg_rat = 0.35
                             cell.h[1,0,1] = 0.6*preg_rat
                         elif cell.preg == 'mid':
-                            preg_rat = 0.6 
+                            preg_rat = 0.7 
                             cell.h[1,0,1] = 0.6*preg_rat
                 elif cell.segment == 'CNT':
                     if cell.preg == 'late':
                         preg_rat = 0.35
                         cell.h[1,0,1] = 8.0*preg_rat
                     elif cell.preg == 'mid':
-                        preg_rat = 0.6 
+                        preg_rat = 0.7 
                         cell.h[1,0,1] = 8.0*preg_rat
                 elif cell.segment == 'CCD':
                     if cell.preg == 'late':
                         preg_rat = 0.55
                         cell.h[1,0,1] = 2.8*preg_rat
                     elif cell.preg == 'mid':
-                        preg_rat = 0.7 
+                        preg_rat = 0.8 
                         cell.h[1,0,1] = 2.8*preg_rat
                 elif cell.segment == 'OMCD':
                     if cell.preg == 'late':
                         preg_rat = 0.55
                         cell.h[1,0,1] = 2.4*preg_rat
                     elif cell.preg == 'mid':
-                        preg_rat = 0.7 
+                        preg_rat = 0.8 
                         cell.h[1,0,1] = 2.4*preg_rat
                     
                             
