7c7709cac0db4f3ee0f4b046949559d406d910b0
Melissa Stadt <84604412+mstadt@users.noreply.github.com>
2021-10-19 18:26:47 -0400

    Update Newton_preg.py

--- Newton_preg.py
+++ Newton_preg.py
@@ -98,7 +98,7 @@ def newton_preg_rat(func,x,k,cell):
             if np.linalg.norm(f)>5000:
                 if cell.preg == 'mid':
                     if cell.type == 'jux2':
-                        amp = 0.4
+                        amp = 0.25
                     elif cell.type == 'jux3':
                         amp = 0.4
                     elif cell.type == 'jux4':
