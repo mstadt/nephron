adc75e34f85c24f1de4d2369967c59c238e7efd0
Melissa Stadt <84604412+mstadt@users.noreply.github.com>
2021-10-23 23:46:09 -0400

    Update set_params_preg.py

--- set_params_preg.py
+++ set_params_preg.py
@@ -441,12 +441,12 @@ def read_params_preg(cell,filename,j):
                         if cell.preg == 'mid':
                             newdLA.perm = 1.125*newdLA.perm
                         elif cell.preg == 'late':
-                            newdLA.perm = 1.125*newdLA.perm
+                            newdLA.perm = 1.1*newdLA.perm
                     elif cell.segment == 'IMCD':
                         if cell.preg == 'mid':
                             newdLA.perm = 1.125*newdLA.perm
                         elif cell.preg == 'late':
-                            newdLA.perm = 1.125*newdLA.perm
+                            newdLA.perm = 1.1*newdLA.perm
                     else:
                         print('segment: ' + cell.segment)
                         raise Exception('Na-Cl coupled transporter not characterized for pregnancy in this segment')
@@ -512,7 +512,7 @@ def read_params_preg(cell,filename,j):
                         if cell.preg == 'mid':
                             preg_rat = 1.2
                         elif cell.preg == 'late':
-                            preg_rat = 1.0
+                            preg_rat = 1.05
                     else:
                         print('segment: ' + cell.segment)
                         raise Exception('NaKATPase activity not done for pregnancy in segment')
