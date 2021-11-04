9a96faa6dec81ca5125d04d490b3db8001ead70e
Melissa Stadt <84604412+mstadt@users.noreply.github.com>
2021-11-04 16:00:13 -0400

    Update Newton_preg.py

--- Newton_preg.py
+++ Newton_preg.py
@@ -172,8 +172,10 @@ def newton_preg_rat(func,x,k,cell):
                         amp = 0.4
                     else:
                         amp = 0.5
+                elif np.linalg.norm(f)>2000:
+                    amp = 0.75 
                 elif np.linalg.norm(f)>1000:
-                    amp = 0.95 
+                    amp = 0.8
                 elif iter > 50:
                     amp = 0.95
                 elif iter > 100:
