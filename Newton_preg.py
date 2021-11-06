4ff1104d82da2bf0e1a07b36057a2ade03b731e9
Melissa Stadt <84604412+mstadt@users.noreply.github.com>
2021-11-06 17:20:45 -0400

    Update Newton_preg.py

--- Newton_preg.py
+++ Newton_preg.py
@@ -96,7 +96,7 @@ def newton_preg_rat(func,x,k,cell):
         # CNT
         elif cell.segment == 'CNT':
             if np.linalg.norm(f)>1e6:
-                amp = 0.3
+                amp = 0.35
             elif np.linalg.norm(f)>5000:
                 if cell.preg == 'mid':
                     if cell.type == 'jux1':
@@ -115,6 +115,9 @@ def newton_preg_rat(func,x,k,cell):
                     else:
                         amp = 0.5
                 elif cell.preg == 'late':
+                    if cell.type == 'jux1':
+                        amp = 0.8
+                    else:
                         amp = 0.65
             elif np.linalg.norm(f)>2000:
                 if cell.preg == 'mid':
