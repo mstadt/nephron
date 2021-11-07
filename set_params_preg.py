03dc82704796a23da6a617deb5d909cbae065fca
Melissa Stadt <84604412+mstadt@users.noreply.github.com>
2021-11-07 18:24:39 -0500

    Update set_params_preg.py

--- set_params_preg.py
+++ set_params_preg.py
@@ -365,28 +365,28 @@ def read_params_preg(cell,filename,j):
                             preg_rat = 0.4
                             cell.h[1,0,1] = 0.6*preg_rat
                         elif cell.preg == 'mid':
-                            preg_rat = 0.6 
+                            preg_rat = 0.575 
                             cell.h[1,0,1] = 0.6*preg_rat
                 elif cell.segment == 'CNT':
                     if cell.preg == 'late':
                         preg_rat = 0.4
                         cell.h[1,0,1] = 8.0*preg_rat
                     elif cell.preg == 'mid':
-                        preg_rat = 0.6 
+                        preg_rat = 0.575 
                         cell.h[1,0,1] = 8.0*preg_rat
                 elif cell.segment == 'CCD':
                     if cell.preg == 'late':
                         preg_rat = 0.65
                         cell.h[1,0,1] = 2.8*preg_rat
                     elif cell.preg == 'mid':
-                        preg_rat = 0.75 
+                        preg_rat = 0.725 
                         cell.h[1,0,1] = 2.8*preg_rat
                 elif cell.segment == 'OMCD':
                     if cell.preg == 'late':
                         preg_rat = 0.65
                         cell.h[1,0,1] = 2.4*preg_rat
                     elif cell.preg == 'mid':
-                        preg_rat = 0.75 
+                        preg_rat = 0.725 
                         cell.h[1,0,1] = 2.4*preg_rat
                     
                             
@@ -538,7 +538,7 @@ def read_params_preg(cell,filename,j):
                         preg_rat = 2.15
                 elif newTransp.type == 'HKATPase':
                     if cell.preg == 'mid':
-                        preg_rat = 2.0
+                        preg_rat = 2.05
                     elif cell.preg == 'late':
                         preg_rat = 2.75
                 elif newTransp.type == 'HATPase':
