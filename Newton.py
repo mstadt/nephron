98194c8fce4a79236ad038e96f96f66013dd539a
Melissa Stadt <84604412+mstadt@users.noreply.github.com>
2021-11-15 15:30:03 -0500

    Update Newton.py

--- Newton.py
+++ Newton.py
@@ -181,28 +181,28 @@ def newton_rat(func,x,k,cell):
                 if cell.type == 'jux1':
                     amp = 0.25
                 elif cell.type == 'jux2':
-                    amp = 0.25
+                    amp = 0.35
                 else:
                     amp = 0.5
             elif np.linalg.norm(f)>1e5:
                 if cell.type == 'jux1':
                     amp = 0.5
                 elif cell.type == 'jux2':
-                    amp = 0.5
+                    amp = 0.65
                 else:
                     amp = 1.0
             elif np.linalg.norm(f)>5000:
                 if cell.type == 'jux1':
                     amp = 0.5
                 elif cell.type == 'jux2':
-                    amp = 0.5
+                    amp = 0.75
                 else:
                     amp = 1.0
             elif np.linalg.norm(f)>2500:
                 if cell.type == 'jux1':
                     amp = 0.5
                 elif cell.type == 'jux2':
-                    amp = 0.5
+                    amp = 0.85
                 else:
                     amp = 1.0
             else:
