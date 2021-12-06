25f80e72f875572add83a0d95fbe73cc7d64e4af
Melissa Stadt <84604412+mstadt@users.noreply.github.com>
2021-12-06 16:50:19 -0500

    Update Newton.py

--- Newton.py
+++ Newton.py
@@ -183,7 +183,7 @@ def newton_rat(func,x,k,cell):
                 elif cell.type == 'jux2':
                     amp = 0.35
                 elif cell.type == 'jux3':
-                    amp = 0.45
+                    amp = 0.35
                 elif cell.type == 'sup':
                     if cell.HT == 'Y':
                         amp = 0.3
