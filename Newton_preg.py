e4740cdade9f87c6dfa70f5c0abb08c4403b32aa
Melissa Stadt <84604412+mstadt@users.noreply.github.com>
2022-01-27 09:49:49 -0500

    Update Newton_preg.py

--- Newton_preg.py
+++ Newton_preg.py
@@ -95,7 +95,7 @@ def newton_preg_rat(func,x,k,cell):
         # DCT
         elif cell.segment == 'DCT':
             if np.linalg.norm(f)>1e7:
-                amp = 0.5
+                amp = 0.8
             else:
                 amp = 1.0
         # CNT
