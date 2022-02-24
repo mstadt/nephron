18be1d0f5f978950b9ee4fc9b1b265826457c40e
Melissa Stadt <84604412+mstadt@users.noreply.github.com>
2022-02-24 15:09:10 -0500

    Update set_params_preg.py

--- set_params_preg.py
+++ set_params_preg.py
@@ -587,7 +587,7 @@ def read_params_preg(cell,filename,j):
                         elif cell.preg == 'late':
                             preg_rat = 1.5
                         if cell.HT != 'N':
-                            HT_rat = 0.45
+                            HT_rat = 0.7
                             preg_rat = HT_rat*preg_rat
                         if cell.inhib == 'NKCC2-70':
                             preg_rat = preg_rat*(1-0.7)
@@ -603,7 +603,7 @@ def read_params_preg(cell,filename,j):
                         if cell.inhib == 'NKCC2-100':
                             preg_rat = preg_rat*(1-1)
                         if cell.HT != 'N':
-                            HT_rat = 1.20
+                            HT_rat = 1.74
                             preg_rat = HT_rat*preg_rat
                     else:
                         print('segment: ' + cell.segment)
