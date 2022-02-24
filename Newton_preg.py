25e420c0a7dd10c2af6e6b71ef03230d80625523
Melissa Stadt <84604412+mstadt@users.noreply.github.com>
2022-02-24 18:00:58 -0500

    Update Newton_preg.py

--- Newton_preg.py
+++ Newton_preg.py
@@ -350,7 +350,7 @@ def newton_preg_rat(func,x,k,cell):
 
         # extra check
         if np.isnan(np.linalg.norm(f)) or np.linalg.norm(f)>1e14:
-            amp = amp/3
+            amp = amp/2 #amp/3
             delta = amp*np.array(F*IJ.T)[0]
             x = xold - delta
             f = np.matrix(fun(x,k))
