ec97e03d65de8710111047a73325379ebe9432a6
Melissa Stadt <84604412+mstadt@users.noreply.github.com>
2022-05-12 13:48:29 -0400

    Update Newton_preg.py

--- Newton_preg.py
+++ Newton_preg.py
@@ -254,7 +254,7 @@ def newton_preg_rat(func,x,k,cell):
                             amp = 0.3
                     elif cell.type == 'jux3':
                         if k==0:
-                            amp = 0.5
+                            amp = 1.0 #0.5
                         else:
                             amp = 0.25
                     elif cell.type == 'jux4':
